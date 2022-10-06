eef3a56740854e5369c919cc0f87ae84e47680cb
chetanhiwase12345 <110452195+chetanhiwase12345@users.noreply.github.com>
2022-10-07 01:07:14 +0530

    Add files via upload

---
+++ DepartmentApplication/src/com/departmentinf/department/driver/Driver.java
@@ -0,0 +1,28 @@
+package com.departmentinf.department.driver;
+import com.departmentinf.departments.model.AdminDepartment;
+import com.departmentinf.departments.model.HrDepartment;
+import com.departmentinf.departments.model.TechDepartment;
+public class Driver {
+	public static void main(String[] args)
+	{
+		AdminDepartment ad = new AdminDepartment();
+		System.out.println("Welcome to " + ad.departmentName());
+		System.out.println(ad.getTodaysWork());
+		System.out.println(ad.getWorkDeadline());
+		System.out.println(ad.isTodayAHoliday());
+		System.out.println();
+		HrDepartment hr = new HrDepartment();
+		System.out.println("Welcome to " + hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday());
+		System.out.println();
+		TechDepartment tech = new TechDepartment();
+		System.out.println("Welcome to " + tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+	}
+}
---
+++ DepartmentApplication/src/com/departmentinf/departments/model/AdminDepartment.java
@@ -0,0 +1,12 @@
+package com.departmentinf.departments.model;
+public class AdminDepartment extends SuperDepartment{
+	public String departmentName() {
+		return "Admin Department";		
+	}
+	public String getTodaysWork() {
+		return "Complete your documents submission";	
+	}
+	public String getWorkDeadline() {
+		return "Complete by EOD";	
+	}
+}
